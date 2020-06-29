e6570074a3424602075317b540a2291c21476c37
Lizette Leon <lizetteleon@Lizettes-Air.attlocal.net>
2020-06-28 19:30:52 -0700

    fixing issues with icon generation

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Weather-Dashboard
+
+<!DOCTYPE html>
+
+<html lang="en-us">
+    <head>
+        <meta charset="UTF-8" />
+    </head>
+    <header>
+        <h2>
+            Description
+        </h2>
+            <p>
+                In this project I have created 
+            </p>
+        <h2>
+            Technologies Used:
+        </h2>   
+            <p>
+                HTML, CSS, Javascript, APIS
+            </p>
+    <header>
+    <section>
+      <img src="">
+    </section>
+    <section>
+        <p>
+            The URL of the deployed application
+            <a href="">The URL of the deployed application</a>
+        </p>
+    </section>
+    <section>
+        <p>
+            The URL of the GitHub repository 
+            <a href="">The URL of the Git</a>
+        </p>
+    </section>
+    
--- assets/js/script.js
+++ assets/js/script.js
@@ -89,23 +89,263 @@ function uvIndex(lon, lat) {
 		$(".uvIndex").append(uvBtn);
 		// UV Index changes color depending on level from 0-11
 		if (uvFinal < 3) {
-			// IF RETURN IS 0-2 SYLE GREEN
-			uvBtn.attr("class", "uvGreen");
+            uvBtn.attr("class", "uvGreen");
 		} else if (uvFinal < 6) {
-			// IF 3-5 STYLE YELLOW
 			uvBtn.attr("class", "uvYellow");
 		} else if (uvFinal < 8) {
-			// IF 6-7 STYLE ORANGE
 			uvBtn.attr("class", "uvOrange");
 		} else if (uvFinal < 11) {
-			// IF 8-10 STYLE RED
 			uvBtn.attr("class", "uvRed");
 		} else {
-			// IF 11+ STYLE VIOLET
 			uvBtn.attr("class", "uvPurple");
 		}
 	});
 }
 
+function renderButtons() {
+	$(".list-group").empty();
 
+	// Looping through the array of cities
+	for (var i = 0; i < citiesArray.length; i++) {
+		var a = $("<li>");
+		a.addClass("cityName");
+		a.addClass("list-group-item");
+		a.attr("data-name", citiesArray[i]);
+		a.text(citiesArray[i]);
+		$(".list-group").append(a);
+	}
 
+	$(".cityName").on("click", function(event) {
+		event.preventDefault();
+		var city = $(this).data("name");
+		fiveDay(city);
+		citySearch(city);
+	});
+}
+
+function fiveDay(city) {
+	var fiveFront = "https://api.openweathermap.org/data/2.5/forecast?q=";
+	var fiveURL = fiveFront + city + APIKey;
+	console.log(fiveURL);
+
+	//clear out previous data
+	$(".card-text").empty();
+	$(".card-title").empty();
+
+	$.ajax({
+		url: fiveURL,
+		method: "GET"
+	}).then(function(response) {
+		//dates
+		var dateOne = moment
+			.unix(response.list[1].dt)
+			.utc()
+			.format("L");
+		$(".dateOne").append(dateOne);
+		var dateTwo = moment
+			.unix(response.list[9].dt)
+			.utc()
+			.format("L");
+		$(".dateTwo").append(dateTwo);
+		var dateThree = moment
+			.unix(response.list[17].dt)
+			.utc()
+			.format("L");
+		$(".dateThree").append(dateThree);
+		var dateFour = moment
+			.unix(response.list[25].dt)
+			.utc()
+			.format("L");
+		$(".dateFour").append(dateFour);
+		var dateFive = moment
+			.unix(response.list[33].dt)
+			.utc()
+			.format("L");
+		$(".dateFive").append(dateFive);
+
+		var iconOne = $("<img>");
+		var iconOneSrc =
+			"https://openweathermap.org/img/wn/" +
+			response.list[4].weather[0].icon +
+			"@2x.png";
+		console.log("card Icon line 280" + iconOneSrc);
+		iconOne.attr("src", iconOneSrc);
+		$(".iconOne").append(iconOne);
+
+		var iconTwo = $("<img>");
+		var iconTwoSrc =
+			"https://openweathermap.org/img/wn/" +
+			response.list[12].weather[0].icon +
+			"@2x.png";
+		iconTwo.attr("src", iconTwoSrc);
+		$(".iconTwo").append(iconTwo);
+
+		var iconThree = $("<img>");
+		var iconThreeSrc =
+			"https://openweathermap.org/img/wn/" +
+			response.list[20].weather[0].icon +
+			"@2x.png";
+		iconThree.attr("src", iconThreeSrc);
+		$(".iconThree").append(iconThree);
+
+		var iconFour = $("<img>");
+		var iconFourSrc =
+			"https://openweathermap.org/img/wn/" +
+			response.list[28].weather[0].icon +
+			"@2x.png";
+		iconFour.attr("src", iconFourSrc);
+		$(".iconFour").append(iconFour);
+
+		var iconFive = $("<img>");
+		var iconFiveSrc =
+			"https://openweathermap.org/img/wn/" +
+			response.list[36].weather[0].icon +
+			"@2x.png";
+		iconFive.attr("src", iconFiveSrc);
+        $(".iconFive").append(iconFive);
+        
+		$(".tempOne").append("Temperature: ");
+		$(".tempOne").append(
+			tempAvg(
+				response.list[2].main.temp,
+				response.list[4].main.temp,
+				response.list[6].main.temp
+			)
+		);
+		$(".tempOne").append(" °F");
+
+		$(".tempTwo").append("Temperature: ");
+		$(".tempTwo").append(
+			tempAvg(
+				response.list[10].main.temp,
+				response.list[12].main.temp,
+				response.list[14].main.temp
+			)
+		);
+		$(".tempTwo").append(" °F");
+
+		$(".tempThree").append("Temperature: ");
+		$(".tempThree").append(
+			tempAvg(
+				response.list[18].main.temp,
+				response.list[20].main.temp,
+				response.list[22].main.temp
+			)
+		);
+		$(".tempThree").append(" °F");
+
+		$(".tempFour").append("Temperature: ");
+		$(".tempFour").append(
+			tempAvg(
+				response.list[26].main.temp,
+				response.list[28].main.temp,
+				response.list[30].main.temp
+			)
+		);
+		$(".tempFour").append(" °F");
+
+		$(".tempFive").append("Temperature: ");
+		$(".tempFive").append(
+			tempAvg(
+				response.list[34].main.temp,
+				response.list[36].main.temp,
+				response.list[38].main.temp
+			)
+		);
+        $(".tempFive").append(" °F");
+        
+		$(".humidityOne").append("Humidity: ");
+		$(".humidityOne").append(
+			humidityAvg(
+				response.list[2].main.humidity,
+				response.list[4].main.humidity,
+				response.list[6].main.humidity
+			)
+		);
+		$(".humidityOne").append("%");
+
+		$(".humidityTwo").append("Humidity: ");
+		$(".humidityTwo").append(
+			humidityAvg(
+				response.list[10].main.humidity,
+				response.list[12].main.humidity,
+				response.list[14].main.humidity
+			)
+		);
+		$(".humidityTwo").append("%");
+
+		$(".humidityThree").append("Humidity: ");
+		$(".humidityThree").append(
+			humidityAvg(
+				response.list[18].main.humidity,
+				response.list[20].main.humidity,
+				response.list[22].main.humidity
+			)
+		);
+		$(".humidityThree").append("%");
+
+		$(".humidityFour").append("Humidity: ");
+		$(".humidityFour").append(
+			humidityAvg(
+				response.list[26].main.humidity,
+				response.list[28].main.humidity,
+				response.list[30].main.humidity
+			)
+		);
+		$(".humidityFour").append("%");
+
+		$(".humidityFive").append("Humidity: ");
+		$(".humidityFive").append(
+			humidityAvg(
+				response.list[34].main.humidity,
+				response.list[36].main.humidity,
+				response.list[38].main.humidity
+			)
+		);
+		$(".humidityFive").append("%");
+	});
+}
+
+function tempAvg(x, y, z) {
+	var avgThree = (x + y + z) / 3.0;
+	var avgReturn = ((avgThree - 273.15) * 1.8 + 32).toFixed(0);
+	return avgReturn;
+}
+
+function humidityAvg(x, y, z) {
+	var avgHum = (x + y + z) / 3.0;
+	return avgHum.toFixed(0);
+}
+
+// when a city is entered and searched the entry is saved in local storage and generates a result in 
+$("#add-city").on("click", function(event) {
+	event.preventDefault();
+	
+	var city = $("#city-input")
+		.val()
+		.trim();
+
+	var containsCity = false;
+
+	if (citiesArray != null) {
+		$(citiesArray).each(function(x) {
+			if (citiesArray[x] === city) {
+				containsCity = true;
+			}
+		});
+	}
+
+	if (containsCity === false) {
+		citiesArray.push(city);
+	}
+
+	localStorage.setItem("cities", JSON.stringify(citiesArray));
+
+	fiveDay(city);
+
+	citySearch(city);
+
+	renderButtons();
+});
+
+renderButtons();
